dc2211541f991e5052da3bce756456d7c89955f5
jananiram05 <jpjanani5@gmail.com>
2023-10-27 13:14:22 -0400

    First commit

--- dfsdfsdfsfds/src/test/java/com/utility/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.utility;
-
-public class Constants {
-	public static final String USER_DIR = System.getProperty("user.dir");
-	public static final String APPLICATION_PROPERTIES = USER_DIR + "/src/test/resources/applicationData.properties";
-         
-}
